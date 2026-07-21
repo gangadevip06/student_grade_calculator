71dd88b9347970466ed86441e2693b6204fa53da
Ganga Devi <gangadevip06@gmail.com>
2026-07-21 23:21:04 +0530

    Added Backend javascript file

---
+++ grade.js
@@ -0,0 +1,57 @@
+body{
+    font-family: Arial, sans-serif;
+    background:#f4f4f4;
+}
+
+.container{
+    width:700px;
+    margin:30px auto;
+    background:white;
+    padding:20px;
+    border-radius:10px;
+    box-shadow:0 0 10px gray;
+}
+
+h2{
+    text-align:center;
+}
+
+input{
+    width:97%;
+    padding:10px;
+    margin:6px 0;
+}
+
+button{
+    width:100%;
+    padding:10px;
+    background:green;
+    color:white;
+    border:none;
+    cursor:pointer;
+    font-size:16px;
+}
+
+button:hover{
+    background:darkgreen;
+}
+
+table{
+    width:100%;
+    margin-top:20px;
+    border-collapse:collapse;
+}
+
+table,th,td{
+    border:1px solid black;
+}
+
+th{
+    background:#4CAF50;
+    color:white;
+}
+
+th,td{
+    padding:10px;
+    text-align:center;
+}
